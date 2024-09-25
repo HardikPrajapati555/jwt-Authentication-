cdb23f07e5c1b63e53d2fc18e26de5f72f332a8f
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-09-25 17:46:23 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-const Role = require('./Role');
+const Role = require('./role');
 const Permission = require('./Permission');
 
 const User = sequelize.define('User', {
@@ -18,7 +18,7 @@ const User = sequelize.define('User', {
     allowNull: false,                                                   
   },
 });
-                                                                                        
+
 User.belongsTo(Role);
 User.hasOne(Permission);
 
