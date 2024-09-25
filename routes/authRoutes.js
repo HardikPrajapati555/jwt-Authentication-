634924ba1af711ddd550080de51624d9e0ef7c76
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-09-25 17:25:09 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const { register, login } = require('../controllers/authController');
 
 router.get('/register', (req, res) => res.render('register'));
 router.post('/register', register);
-router.get('/login', (req, res) => res.render('login'));
+router.get('/', (req, res) => res.render('login'));
 router.post('/login', login);
 
 module.exports = router;
