c6a2bd3c7d280536085d51c5f6fb3d9c1e5d8840
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-09-25 17:40:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
+require('dotenv').config(); // Load environment variables from .env
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('form', 'postgres', 'hardik', {
-  host: 'localhost',
+// Using environment variables for database connection
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: process.env.SSL === 'true', // Enable SSL if specified in .env
+      rejectUnauthorized: false // Set to false to avoid self-signed certificate issues
+    }
+  },
   logging: false, // Disable logging for production
 });
 
-// sequelize.sync(); // Sync models with DB
+// sequelize.sync(); // Uncomment if you want to sync models with the DB
 
 module.exports = sequelize;
-
-    
-
-    // module.exports= function
